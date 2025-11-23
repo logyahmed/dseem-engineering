02cd3b9c262da0079321b550bb8b0c20088df5bf
ahmed eraky ahmed <93797403+logyahmed@users.noreply.github.com>
2025-11-23 16:59:15 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
-const __dirname = new URL(".", import.meta.url).pathname;
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  plugins: [react()],
-
-  // جذر المشروع الحقيقي هو مكان index.html
-  root: __dirname,
-
+  plugins: [
+    react(),
+    runtimeErrorOverlay(),
+    ...(process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
+      ? [
+          await import("@replit/vite-plugin-cartographer").then((m) =>
+            m.cartographer(),
+          ),
+          await import("@replit/vite-plugin-dev-banner").then((m) =>
+            m.devBanner(),
+          ),
+        ]
+      : []),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@": path.resolve(import.meta.dirname, "client", "src"),
+      "@shared": path.resolve(import.meta.dirname, "shared"),
+      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
-
+  root: path.resolve(import.meta.dirname, "client"),
   build: {
-    // هنا Vite سيخرج المشاريع داخل dist
-    outDir: path.resolve(__dirname, "dist"),
+    outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
-
   server: {
     fs: {
       strict: true,
+      deny: ["**/.*"],
     },
   },
 });
