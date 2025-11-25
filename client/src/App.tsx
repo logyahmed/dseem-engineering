976cbcd96d6d579ae592eea377ddb83c7a3578b7
ahmed eraky ahmed <93797403+logyahmed@users.noreply.github.com>
2025-11-25 17:29:12 +0300

    Update App component structure and imports
    Refactor App component to include multiple sections and clean up exports.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,15 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
+
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import PortfolioGallery from "@/components/PortfolioGallery";
+import AccreditationsSection from "@/components/AccreditationsSection";
+import ContactSection from "@/components/ContactSection";
 
 function Router() {
   return (
@@ -15,32 +22,23 @@ function Router() {
   );
 }
 
-function App() {
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
+
+        {/* موقعك الأساسي */}
+        <HeroSection />
+        <AboutSection />
+        <PortfolioGallery />
+        <AccreditationsSection />
+        <ContactSection />
+
+        {/* لو عندك صفحات أخرى */}
         <Router />
+
       </TooltipProvider>
     </QueryClientProvider>
   );
 }
-
-export default App;
-import AccreditationsSection from "@/components/AccreditationsSection";
-
-// ...
-
-export default function App() {
-  return (
-    <>
-      <HeroSection />
-      <AboutSection />
-      <PortfolioGallery />
-      <AccreditationsSection />   {/* ← اضفها هنا */}
-      <ContactSection />
-      {/* باقي السكاشن */}
-    </>
-  );
-}
-
