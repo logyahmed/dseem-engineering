65e72c800149f110512da3557d70303c0e005c6f
ahmed eraky ahmed <93797403+logyahmed@users.noreply.github.com>
2025-11-27 19:55:14 +0300

    Remove unused components from Router
    Removed unused section components from the Router.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+// client/src/App.tsx
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -6,12 +7,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
-
-import HeroSection from "@/components/HeroSection";
-import AboutSection from "@/components/AboutSection";
-import PortfolioGallery from "@/components/PortfolioGallery";
-import AccreditationsSection from "@/components/AccreditationsSection";
-import ContactSection from "@/components/ContactSection";
 
 function Router() {
   return (
@@ -27,17 +22,7 @@ export default function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-
-        {/* موقعك الأساسي */}
-        <HeroSection />
-        <AboutSection />
-        <PortfolioGallery />
-        <AccreditationsSection />
-        <ContactSection />
-
-        {/* لو عندك صفحات أخرى */}
         <Router />
-
       </TooltipProvider>
     </QueryClientProvider>
   );
